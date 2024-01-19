6a8046c909189e2406d54b2c0568de212a324f46
Leticia Martín <lmartin@facephi.com>
2024-01-19 12:09:33 +0100

    Logo

--- android/v.1.5.2/DemoVideoId/app/build.gradle.kts
+++ android/v.1.5.2/DemoVideoId/app/build.gradle.kts
@@ -8,7 +8,7 @@ android {
     compileSdk = 34
 
     defaultConfig {
-        applicationId = "com.facephi.demovideoid"
+        applicationId = "..."
         minSdk = 24
         targetSdk = 34
         versionCode = 1
