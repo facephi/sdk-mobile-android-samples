9612e7ee628e737a661089f0663d237b74eeb8bd
Leticia Martín <lmartin@facephi.com>
2024-01-19 12:06:06 +0100

    [SM-2660] feat: Demo video Id v.1.5.2

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# sdk-mobile-samples
+# Facephi SDK Mobile samples
 
 In this repository are the programming examples for using the Facephi Mobile SDK.
 
---
+++ android/v.1.5.2/DemoVideoId/app/src/main/java/com/facephi/demovideoid/MainActivity.kt
@@ -0,0 +1,36 @@
+package com.facephi.demovideoid
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.facephi.core.data.SdkApplication
+import com.facephi.demovideoid.ui.theme.DemoVideoIdTheme
+import com.facephi.sdk.SDKController
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            DemoVideoIdTheme {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    MainScreen(sdkApplication = SdkApplication(application), modifier = Modifier.fillMaxSize())
+                }
+            }
+        }
+    }
+
+    override fun onDestroy() {
+        SDKController.closeSession()
+        super.onDestroy()
+    }
+}
