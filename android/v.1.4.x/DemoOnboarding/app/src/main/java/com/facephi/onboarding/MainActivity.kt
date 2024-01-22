812e694c2b6588aa7a3e36107049182a3bea4689
Leticia Martín <lmartin@facephi.com>
2024-01-22 16:23:29 +0100

    [SM-2670] feat: Demo onboarding v.1.4.0

---
+++ android/v.1.4.x/DemoOnboarding/app/src/main/java/com/facephi/onboarding/MainActivity.kt
@@ -0,0 +1,32 @@
+package com.facephi.onboarding
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.ui.Modifier
+import com.facephi.onboarding.ui.theme.DemoOnboardingTheme
+import com.facephi.sdk.SDKController
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            DemoOnboardingTheme {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    MainScreen(application = application, modifier = Modifier.fillMaxSize())
+                }
+            }
+        }
+    }
+
+    override fun onDestroy() {
+        SDKController.closeSession()
+        super.onDestroy()
+    }
+}
