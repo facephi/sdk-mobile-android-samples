a43cd33244169f960948af40e8300c251ae24bc0
Leticia Martín <lmartin@facephi.com>
2024-03-11 15:22:01 +0100

    [SM-2920] feat: Samples for v.1.5.4

---
+++ android/v.1.5.4/DemoNFC/app/src/main/java/com/facephi/demonfc/ui/composables/AlertDialog.kt
@@ -0,0 +1,69 @@
+package com.facephi.demonfc.ui.composables
+
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.testTag
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.compose.ui.window.DialogProperties
+import com.facephi.sdk_composables.dialogs.AlertDialogData
+
+
+@Composable
+fun AlertDialog(
+    show: Boolean,
+    dialogData: AlertDialogData,
+    onDismiss: () -> Unit,
+    onConfirm: () -> Unit
+) {
+    if (show) {
+        androidx.compose.material.AlertDialog(
+            onDismissRequest = onDismiss,
+            properties = DialogProperties(
+                dismissOnBackPress = false,
+                dismissOnClickOutside = false
+            ),
+            title = {
+                androidx.compose.material3.Text(
+                    modifier = Modifier
+                        .fillMaxWidth(),
+                    text = dialogData.title,
+                    textAlign = TextAlign.Center,
+                    fontWeight = FontWeight.SemiBold,
+                    fontSize = 18.sp
+                )
+
+            },
+            text = {
+                androidx.compose.material3.Text(
+                    modifier = Modifier
+                        .fillMaxWidth(),
+                    text = dialogData.description,
+                    textAlign = TextAlign.Center,
+                    fontWeight = FontWeight.Normal
+                )
+            },
+            confirmButton = {
+                BaseTextButton(
+                    enabled = true,
+                    text = dialogData.confirmText,
+                    modifier = Modifier.padding(bottom = 8.dp),
+                    onClick = onConfirm
+                )
+            },
+            dismissButton = {
+                BaseTextButton(
+                    enabled = true,
+                    text = dialogData.dismissText,
+                    modifier = Modifier,
+                    onClick = onDismiss
+                )
+            },
+            modifier = Modifier.testTag("dialog")
+        )
+    }
+}
