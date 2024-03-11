3c81d48c1a7cc8b5448e69dc815a3695497f7579
Leticia Martín Herrero <85101758+lmartinFacephi@users.noreply.github.com>
2024-03-11 15:35:31 +0100

    [SM-2920] feat: Samples for v.1.5.4 (#14)

---
+++ android/v.1.5.4/DemoVoice/app/src/main/java/com/facephi/demovoice/media/AppMediaPlayer.kt
@@ -0,0 +1,60 @@
+package com.facephi.demovoice.media
+
+import android.media.MediaPlayer
+import io.github.aakira.napier.Napier
+
+object AppMediaPlayer {
+    private var mediaPlayer = MediaPlayer()
+
+    private var audios: List<String>? = null
+    private var audioIndex = 0
+    private lateinit var indexOutput: (Int) -> Unit
+    private lateinit var onStop: () -> Unit
+
+    fun init(audios: List<String>, indexOutput: (Int) -> Unit, onStop: () -> Unit) {
+        AppMediaPlayer.audios = audios
+        audioIndex = 0
+        AppMediaPlayer.indexOutput = indexOutput
+        AppMediaPlayer.onStop = onStop
+    }
+
+    private fun play(file: String, onFinish: () -> Unit) {
+        Napier.d("APP: MEDIA PLAY file:$file")
+        // val mediaSource = AppMediaDataSource(byteArray)
+        mediaPlayer = MediaPlayer()
+        mediaPlayer.apply {
+            setDataSource(file)
+            setOnCompletionListener {
+                onFinish()
+                release()
+            }
+            prepareAsync()
+            setOnPreparedListener {
+                indexOutput.invoke(audioIndex)
+                start()
+            }
+        }
+    }
+
+    fun stop() {
+        Napier.d("APP: MEDIA STOP")
+
+        audioIndex = 0
+        if (mediaPlayer.isPlaying == true) {
+            mediaPlayer.stop()
+            mediaPlayer.release()
+        }
+    }
+
+    fun playAudios() {
+        if (audioIndex < audios!!.size) {
+            play(audios!![audioIndex]) {
+                audioIndex += 1
+                playAudios()
+            }
+        } else {
+            stop()
+            onStop()
+        }
+    }
+}
