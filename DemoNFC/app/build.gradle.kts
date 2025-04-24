c322da8321f9526fd32978a831c36a0ae645425e
Leticia Martín <lmartin@facephi.com>
2025-04-24 17:02:20 +0200

    [SM-4864] feat: SDK 2.3.0

--- DemoNFC/app/build.gradle.kts
+++ DemoNFC/app/build.gradle.kts
@@ -9,7 +9,7 @@ plugins {
 
 android {
     namespace = "com.facephi.demonfc"
-    compileSdk = 34
+    compileSdk = 35
 
     buildFeatures {
         buildConfig = true
@@ -18,7 +18,7 @@ android {
     defaultConfig {
         applicationId = "..."
         minSdk = 24
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
