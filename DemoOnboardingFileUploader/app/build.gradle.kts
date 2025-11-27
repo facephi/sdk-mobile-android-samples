2deb66b3f910a18a4faca1a1936a76f56716bed0
Leticia Martín <lmartin@facephi.com>
2025-11-27 13:21:14 +0100

    [SM-6745] feat: DemoOnboardingFileUploader

---
+++ DemoOnboardingFileUploader/app/build.gradle.kts
@@ -0,0 +1,85 @@
+plugins {
+    alias(libs.plugins.androidApplication)
+    alias(libs.plugins.org.jetbrains.kotlin.android)
+    alias(libs.plugins.compose.compiler)
+    alias(libs.plugins.jetbrainsCompose)
+    alias(libs.plugins.kotlin.parcelize)
+    alias(libs.plugins.kotlin.serialization)
+}
+
+android {
+    namespace = "com.facephi.onboarding"
+    compileSdk = 36
+
+    buildFeatures {
+        buildConfig = true
+    }
+
+    defaultConfig {
+        applicationId = "..."
+        minSdk = 24
+        targetSdk = 36
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        vectorDrawables {
+            useSupportLibrary = true
+        }
+        buildConfigField( "String", "LIBRARY_VERSION", "\"${libs.versions.facephiVersion.get()}\"")
+    }
+
+    buildTypes {
+        release {
+            isShrinkResources = true
+            isMinifyEnabled = true
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+            signingConfig = signingConfigs.getByName("debug")
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+    buildFeatures {
+        compose = true
+    }
+    packaging {
+        resources {
+            excludes += "/META-INF/{AL2.0,LGPL2.1}"
+        }
+    }
+}
+
+dependencies {
+    implementation(libs.core.ktx)
+    implementation(libs.lifecycle.runtime.ktx)
+    implementation(libs.activity.compose)
+    implementation(libs.lifecycle.viewmodel.compose)
+    implementation(compose.ui)
+    implementation(compose.preview)
+    implementation(compose.material3)
+    implementation(libs.kotlinx.datetime)
+    debugImplementation(compose.uiTooling)
+
+    // Ktor Client Http
+    implementation(libs.ktor.client.okhttp)
+    implementation(libs.ktor.serialization.kotlinx.json)
+    implementation(libs.ktor.client.logging)
+    implementation(libs.ktor.client.content.negotiation)
+
+    // SDK Facephi
+    implementation (libs.facephi.sdk)
+    implementation (libs.facephi.selphid)
+    implementation (libs.facephi.selphi)
+    //implementation (libs.facephi.tracking)
+
+    // Logs
+    implementation(libs.napier)
+}
---
+++ DemoOnboardingFileUploader/app/src/main/java/com/facephi/onboarding/MainViewModel.kt
@@ -0,0 +1,283 @@
+package com.facephi.onboarding
+
+import android.content.Context
+import android.graphics.Bitmap
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.facephi.core.data.SdkApplication
+import com.facephi.core.data.SdkImage
+import com.facephi.core.data.SdkResult
+import com.facephi.onboarding.repository.VerificationsApi
+import com.facephi.onboarding.repository.request.AuthenticateFacialRequest
+import com.facephi.onboarding.repository.request.OnboardingRequest
+import com.facephi.onboarding.repository.request.PassiveLivenessTokenRequest
+import com.facephi.onboarding.utils.toBase64
+import com.facephi.sdk.SDKController
+import com.facephi.selphi_component.RawTemplateController
+import com.facephi.selphi_component.SelphiController
+import com.facephi.selphid_component.SelphIDController
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.launch
+import kotlinx.datetime.TimeZone
+import kotlinx.datetime.toLocalDateTime
+import io.github.aakira.napier.Napier
+import kotlin.time.ExperimentalTime
+import kotlin.time.Instant
+
+class MainViewModel : ViewModel() {
+    private val _logs = MutableStateFlow("")
+    val logs = _logs.asStateFlow()
+
+    fun initSdk(sdkApplication: SdkApplication) {
+        viewModelScope.launch {
+            SDKController.getAnalyticsEvents { time, componentName, eventType, info ->
+                Napier.i { "*** ${formatEpochMillis(time)} - ${componentName.name} -" +
+                        " ${eventType.name} -  ${info ?: ""} " }
+            }
+
+            if (BuildConfig.DEBUG) {
+                SDKController.enableDebugMode()
+            }
+
+            val sdkConfig = SdkData.getInitConfiguration(sdkApplication)
+            when (val result = SDKController.initSdk(sdkConfig)) {
+                is SdkResult.Success -> log("INIT SDK OK")
+                is SdkResult.Error -> log("INIT SDK ERROR: ${result.error}")
+            }
+
+            /*SDKController.launch(TrackingErrorController {
+                 log("Tracking Error: ${it.name}")
+             })*/
+        }
+    }
+
+    fun newOperation() {
+        viewModelScope.launch {
+            val result = SDKController.newOperation(
+                operationType = SdkData.OPERATION_TYPE,
+                customerId = SdkData.CUSTOMER_ID,
+            )
+            when (result) {
+                is SdkResult.Success -> log("NEW OPERATION: OK")
+                is SdkResult.Error -> log("NEW OPERATION: Error - ${result.error.name}")
+            }
+        }
+    }
+
+    fun launchSelphi(
+        showTutorial: Boolean,
+        showPreviousTip: Boolean,
+        showDiagnostic: Boolean,
+    ) {
+        viewModelScope.launch {
+            when (val result =
+                SDKController.launch(
+                    SelphiController(
+                        SdkData.getSelphiConfiguration(
+                            showTutorial = showTutorial,
+                            showPreviousTip = showPreviousTip,
+                            showDiagnostic = showDiagnostic
+                        )
+                    )
+                )) {
+                is SdkResult.Success -> {
+                    log("Selphi: OK")
+                    result.data.bestImage?.bitmap?.let {
+                        ImageData.selphiBestImage = it
+                    }
+                    result.data.bestImageTokenized?.let {
+                        ImageData.selphiBestImageTokenized = it
+                    }
+
+                }
+
+                is SdkResult.Error -> log("Selphi: Error - ${result.error.name}")
+            }
+        }
+    }
+
+    fun launchSelphId(
+        showTutorial: Boolean,
+        showPreviousTip: Boolean,
+        showDiagnostic: Boolean,
+    ) {
+        viewModelScope.launch {
+            when (val result =
+                SDKController.launch(
+                    SelphIDController(
+                        SdkData.getSelphIDConfiguration(
+                            showTutorial = showTutorial,
+                            showPreviousTip = showPreviousTip,
+                            showDiagnostic = showDiagnostic
+                        )
+                    )
+                )) {
+                is SdkResult.Success -> {
+                    log("SelphID: OK")
+                    if (result.data.tokenFaceImage.isNotEmpty()) {
+                        ImageData.documentTokenFaceImage = result.data.tokenFaceImage
+                    }
+                    result.data.faceImage?.bitmap.let {
+                        ImageData.documentFace = it
+                    }
+
+                    result.data.frontDocumentImage?.bitmap.let {
+                        ImageData.documentFront = it
+                    }
+
+                    result.data.backDocumentImage?.bitmap.let {
+                        ImageData.documentBack = it
+                    }
+                }
+
+                is SdkResult.Error -> log("SelphID: Error - ${result.error.name}")
+            }
+        }
+    }
+
+    fun generateTemplateRawFromBitmap(bitmap: Bitmap) {
+        viewModelScope.launch {
+            when (val result = SDKController.launch(RawTemplateController(SdkImage(bitmap)))) {
+                is SdkResult.Success -> log("Template generated. Length: ${result.data.result.length}")
+                is SdkResult.Error -> log("Template: Error - ${result.error.name}")
+            }
+        }
+    }
+
+    private fun log(message: String) {
+        viewModelScope.launch {
+            val data = _logs.value + "\n" + message
+            _logs.emit(data)
+        }
+
+    }
+
+    fun clearLogs() {
+        viewModelScope.launch {
+            _logs.emit("")
+            ImageData.clear()
+        }
+
+    }
+
+    fun launchVerifications(context: Context) {
+
+        if (SdkData.API_KEY.isEmpty() || SdkData.BASE_URL.isEmpty()) {
+            log("DATA is empty")
+            return
+        }
+
+        val verificationController = VerificationsApi(context, SdkData.API_KEY)
+
+        viewModelScope.launch {
+            // If Tracking Component is used
+            /*val extraData = when (val result = SDKController.launch(ExtraDataController())) {
+                is SdkResult.Success -> result.data
+                is SdkResult.Error -> {
+                    log("EXTRA_DATA: Error - ${result.error}")
+                    ""
+                }
+            }
+
+            if (extraData.isEmpty()) return@launch
+
+            val operationId = SDKController.launch(GetOperationIdController()).orEmpty()
+
+            if (operationId.isEmpty()) return@launch
+
+            val trackingData = TrackingData(
+                extraData = extraData,
+                operationId = operationId
+            )
+
+             */
+
+
+            // LIVENESS WITH TOKENIZED IMAGE
+
+            ImageData.selphiBestImageTokenized?.takeIf { it.isNotBlank() }
+                ?.let { bestImageTokenized ->
+                    val response = verificationController.passiveLivenessToken(
+                        request = PassiveLivenessTokenRequest(
+                            imageBuffer = bestImageTokenized,
+                            //trackingData = trackingData
+                        ),
+                        baseUrl = SdkData.BASE_URL
+                    )
+
+                    log("** passiveLivenessToken: ${response}\n")
+
+                }
+
+            // MATCHING: BASE64 FACE IMAGE AND TOKENIZED DOCUMENT FACE IMAGE
+
+            ImageData.selphiBestImage?.toBase64()?.takeIf { it.isNotBlank() }?.let { bestImageB64 ->
+                ImageData.documentTokenFaceImage?.takeIf { it.isNotBlank() }
+                    ?.let { documentTokenFaceImage ->
+                        val response = verificationController.authenticateFacial(
+                            request = AuthenticateFacialRequest(
+                                token1 = bestImageB64,
+                                token2 = documentTokenFaceImage,
+                                method = 4,
+                                //trackingData = trackingData
+                            ),
+                            baseUrl = SdkData.BASE_URL
+                        )
+
+                        log("** authenticateFacial (method = 4): ${response}\n")
+                    }
+            }
+
+            // MATCHING: BASE64 FACE IMAGE AND TOKENIZED DOCUMENT FACE IMAGE
+
+            ImageData.selphiBestImageTokenized?.takeIf { it.isNotBlank() }
+                ?.let { bestImageTokenized ->
+                    ImageData.documentTokenFaceImage?.takeIf { it.isNotBlank() }
+                        ?.let { documentTokenFaceImage ->
+                            val response = verificationController.authenticateFacial(
+                                request = AuthenticateFacialRequest(
+                                    token1 = bestImageTokenized,
+                                    token2 = documentTokenFaceImage,
+                                    method = 5,
+                                    //trackingData = trackingData
+                                ),
+                                baseUrl = SdkData.BASE_URL
+                            )
+
+                            log("** authenticateFacial (method = 5): ${response}\n")
+                        }
+                }
+
+            // ONBOARDING: BASE64 FACE IMAGE AND TOKENIZED DOCUMENT FACE IMAGE
+
+            ImageData.selphiBestImageTokenized?.takeIf { it.isNotBlank() }
+                ?.let { bestImageTokenized ->
+                    ImageData.documentTokenFaceImage?.takeIf { it.isNotBlank() }
+                        ?.let { documentTokenFaceImage ->
+                            val response = verificationController.onboarding(
+                                request = OnboardingRequest(
+                                    bestImageToken = bestImageTokenized,
+                                    token1 = documentTokenFaceImage,
+                                    method = 5,
+                                    //trackingData = trackingData
+                                ),
+                                baseUrl = SdkData.BASE_URL
+                            )
+
+                            log("** onboarding (method = 5): ${response}\n")
+                        }
+                }
+        }
+
+    }
+
+    @OptIn(ExperimentalTime::class)
+    private fun formatEpochMillis(epochMillis: Long): String {
+        val instant = Instant.fromEpochMilliseconds(epochMillis)
+        val localDateTime =
+            instant.toLocalDateTime(TimeZone.currentSystemDefault())
+        return "${localDateTime.date} ${localDateTime.time}"
+    }
+
+}
